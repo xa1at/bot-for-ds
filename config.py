7ec61aaf24930a5ddf792d38ff550f9433c7f702
xa1at <82275454+xa1at@users.noreply.github.com>
2021-07-11 20:47:42 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'TOKEN': 'ODYzNDkzNzE2NzMxODg3NjQ3.YOntMQ.v8u5tbPeg0SRNHOd6-K3Sisln_Y', # Получение токена - https://prnt.sc/uofhur
+	'NAME BOT': 'alimka', # Имя вашего бота
+	'ID': '863493716731887647', # Получение ID - https://prnt.sc/uofik2
+	'PREFIX': '-', # Префикс
+	'OWNER':'alimka#1111',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
+	'OWNER NAME':'alimka' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
+}
